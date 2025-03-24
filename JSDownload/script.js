9b27985fd1cacc27453dc8d0177c3355c35b7141
Erin Livingston <elivingston@gradcenter.cuny.edu>
2025-03-24 11:56:24 -0400

    interactivity lesson

--- JSDownload/script.js
+++ JSDownload/script.js
@@ -1,1 +1,22 @@
 alert('My project is going to be awesome!');
+$("#noShow").hide();
+$("h1").html("current thoughts"); // changes all h1 tags on the page
+
+$("#love").append("<p> More thoughts loading ... <br /> just kidding, too sleepy")
+
+//modify css styles for a button
+$("#toggleThoughts").css({
+    "background-color": "yellow",
+    "color": "black",
+    "font-size": "20px",
+    "padding": "10px",
+    "border-radius": "5px",
+    "border": "2px solid black",
+    "margin": "10px"
+});
+// preferred to style within the CSS stylesheet 
+// but JavaScript is capable too 
+
+$("#toggleThoughts").click(function() {
+    $("#love").toggle();
+  });
